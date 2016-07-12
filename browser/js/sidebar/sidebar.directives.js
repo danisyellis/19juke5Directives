915d380412e8ae6a736dedf4d54c4dc5bc794523
Danielle Gellis <danisyellis@gmail.com>
2016-07-12 17:51:08 -0400

    the rest of the files

---
+++ browser/js/sidebar/sidebar.directives.js
@@ -0,0 +1,12 @@
+'use strict';
+
+juke.directive("sidebarDir", function() {
+	return {
+		restrict: "E",
+		templateUrl:  "js/sidebar/templates/sidebar.directive.html"
+	};
+});
+
+
+
+//insitde of ng-app and ng-controller the html
